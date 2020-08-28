d42b0a658e0412cefb96db752870d9e7c1de8ec3
Arati Bhandwalkar <arativbhandwalkar@gmail.com>
2020-08-27 17:37:59 -0700

    add email click event

--- src/components/EmployeeDir.js
+++ src/components/EmployeeDir.js
@@ -10,7 +10,7 @@ class EmployeeDir extends Component {
     filteredArr: [],
     sortType: "asc"
   }
-//Loads the data upon pageload
+  //Loads the data upon pageload
   componentDidMount() {
     this.loadData()
   }
@@ -22,23 +22,23 @@ class EmployeeDir extends Component {
       this.setState({
         employees: [...result.data.results],
         filteredArr: [...result.data.results]
-        
+
       })
-      
+
     })
   }
-  
+
   //Takes the input from the form and searches by first and last name based on letters entered
   handleInputChange = event => {
-    
+
     const value = event.target.value;
     let found = this.state.employees.filter(user => user.name.last.toLowerCase().includes(value) || user.name.first.toLowerCase().includes(value))
     console.log(found)
     this.setState({
-      searchTerm:value,
+      searchTerm: value,
       filteredArr: [...found]
     });
-    
+
   };
   //An event handler to sort employees by last name in alternating ascending and descending order.
   handleClickEvent = () => {
@@ -51,7 +51,7 @@ class EmployeeDir extends Component {
       }
       return 0;
     });
-    
+
     let descSort = [...this.state.filteredArr].sort((a, b) => {
       if (a.name.last.toLowerCase() > b.name.last.toLowerCase()) {
         return -1;
@@ -61,57 +61,57 @@ class EmployeeDir extends Component {
       }
       return 0;
     });
-    let emailSort  = [...this.state.filteredArr].sort((a, b) => {
-      if (a.email.toLowerCase() > b.email.toLowerCase()) {
-        return -1;
-      }
-      if (a.email.toLowerCase() < b.email.toLowerCase()) {
-        return 1;
-      }
-      return 0;
-    });
+    // let emailSort  = [...this.state.filteredArr].sort((a, b) => {
+    //   if (a.email.toLowerCase() > b.email.toLowerCase()) {
+    //     return -1;
+    //   }
+    //   if (a.email.toLowerCase() < b.email.toLowerCase()) {
+    //     return 1;
+    //   }
+    //   return 0;
+    // });
 
     //changes the name of the sortType from asc to desc and vice versa, and based on that type will reassign the filtered array
     if (this.state.sortType === "asc") {
       this.setState({ sortType: "desc", filteredArr: descSort }) //if the sortType is asc, turn it to desc and assign descSort to filtered array 
     } else {
-      this.setState({ sortType: "asc", filteredArr: ascSort }) 
+      this.setState({ sortType: "asc", filteredArr: ascSort })
     }
   }
-  
+
   //Takes the information from the API and renders it onto the page in table form.
   render() {
 
     return (
       <>
         <div className="container-fluid">
-        <h1>Employee Directory</h1>
-        <Search searchTerm={this.state.searchTerm} handleInputChange={this.handleInputChange} />
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Photo</th>
+          <h1>Employee Directory</h1>
+          <Search searchTerm={this.state.searchTerm} handleInputChange={this.handleInputChange} />
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Photo</th>
                 <th onClick={() => { this.handleClickEvent() }} scope="col" className={`${this.state.sortType === "asc" ? "stylesAsc" : "stylesDesc"} hover`}>Name</th>
-              <th scope="col">Email</th>
-              <th onClick={() => { this.handleClickEvent() }} scope="col" className={`${this.state.sortType === "asc" ? "stylesAsc" : "stylesDesc"} hover`}>Email</th>
-              <th scope="col">Phone</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.filteredArr.map((employee, index) => {
-              return (
-              <tr key={employee.email}>
-                <th scope="row">{index+1}</th> 
-                <td><img src={employee.picture.medium} alt={employee.name.first} /></td> 
-                <td>{employee.name.first} {employee.name.last}</td>
-                <td>{employee.email}</td>
-                <td>{employee.phone}</td>
+
+                <th onClick={() => { this.handleClickEvent() }} scope="col" className={`${this.state.sortType === "asc" ? "stylesAsc" : "stylesDesc"} hover`}>Email</th>
+                <th scope="col">Phone</th>
               </tr>
-              )
-            })}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.state.filteredArr.map((employee, index) => {
+                return (
+                  <tr key={employee.email}>
+                    <th scope="row">{index + 1}</th>
+                    <td><img src={employee.picture.medium} alt={employee.name.first} /></td>
+                    <td>{employee.name.first} {employee.name.last}</td>
+                    <td>{employee.email}</td>
+                    <td>{employee.phone}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
         </div>
       </>
     );
